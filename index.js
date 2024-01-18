daa9fa2303c73b0f7346fe8d083b5684b0620836
DanielWantsToGoToIvys <123143269+DanielWantsToGoToIvys@users.noreply.github.com>
2024-01-17 19:54:51 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function OverlayOn() {
+    document.getElementById('MenuOverlay').style.display = 'flex';
+}
+function OverlayOff() {
+    document.getElementById("MenuOverlay").style.display = "none";
+}
